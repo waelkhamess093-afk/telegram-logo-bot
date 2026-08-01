07220bab06d759bf1d5ecd61c56508c0ac37433d
waelkhamess093-afk <waelkhamess093@gmail.com>
2026-08-01 04:52:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Telegram Logo Bot is running...")
+from telegram import Update
+from telegram.ext import Application, MessageHandler, ContextTypes, filters
+
+TOKEN = "8536670434:AAGlAwuP5jGYCHYVM09zCwCLg3tItpmstmo""
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("ابعت أي صورة وأنا هضيف اللوجو عليها.")
+
+async def photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("استلمت الصورة 👍")
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(MessageHandler(filters.PHOTO, photo))
+
+print("Bot Started...")
+app.run_polling()
